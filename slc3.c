c15130ce6f0223bfcee67f135986da20fe123007
Sam B <sam2b@uw.edu>
2018-04-16 16:20:27 -0700

    v4.16c Refactoring Necessary
    _the cpu.reg[ ] must NOT be part of memory[ ], and must be its own dedicated array known in the book as the REG FILE.
    _Removed magic numbers with constants defined in the .h file.
    _Some preferred line-up to code blocks).
    _BUG: registers R0..8 still are not updating on display... either that or something else is happening wrong with the command.  INVESTIGATING NOW.

--- slc3.c
+++ slc3.c
@@ -4,7 +4,7 @@
  *  Date Due: Apr 22, 2018
  *  Authors:  Sam Brendel, Tyler Shupack
  *  Problem 3,4
- *  version: 4.16b
+ *  version: 4.16c
  */
 
 #include "slc3.h"
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <ncurses.h>
 
-unsigned short memory[100];
+unsigned short memory[MEMORY_SIZE];
 bool isTrap = false;
 
 /**
@@ -53,19 +53,20 @@ int controller(CPU_p cpu) {
                 cpu.mar = cpu.PC;          // Step 1: MAR is loaded with the contends of the PC,
                 cpu.PC++;                  //         and also increment PC. Only done in the FETCH phase.
                 cpu.mdr = memory[cpu.mar]; // Step 2: Interrogate memory, resulting in the instruction placed into the MDR.
-                cpu.ir = cpu.mdr;          // Step 3: Load the IR with the contents of the MDR.
-                state = DECODE;
+                cpu.ir  = cpu.mdr;          // Step 3: Load the IR with the contents of the MDR.
+                state   = DECODE;
                 break;
 
             case DECODE: // microstate 32
-                opcode = cpu.ir & MASK_OPCODE; // Input is the four-bit opcode IR[15:12]. The output line asserted is the one corresponding to the opcode at the input.
-                opcode = opcode >> 12;
+                opcode = cpu.ir  & MASK_OPCODE; // Input is the four-bit opcode IR[15:12]. The output line asserted is the one corresponding to the opcode at the input.
+                opcode = opcode >> BITSHIFT_OPCODE;
                 switch (opcode) {
                 // different opcodes require different handling
                 // compute effective address, e.g. add sext(immed7) to register.
                 case OP_LD:
-                    dr = cpu.ir >> 9 & 0x7;
-                    offset = cpu.ir & 0x1FF;
+                    dr     = cpu.ir >> BITSHIFT_DR;
+                    dr     = dr      & MASK_SR2;
+                    offset = cpu.ir  & MASK_PCOFFSET9;
                     break;
             }
             state = EVAL_ADDR;
@@ -84,10 +85,10 @@ int controller(CPU_p cpu) {
                     cpu.mdr = memory[cpu.mar]; // microstate 25.
                     break;
                 case OP_ST:
-                    dr = cpu.ir & MASK_DR;         // This is actually a source register, but still use dr.
-                    dr = dr >> 9;
-                    offset = cpu.ir & MASK_PCOFFSET9;
-                    cpu.mar = cpu.PC + offset; // microstate 2.
+                    dr      = cpu.ir  & MASK_DR;         // This is actually a source register, but still use dr.
+                    dr      = dr     >> BITSHIFT_DR;
+                    offset  = cpu.ir  & MASK_PCOFFSET9;
+                    cpu.mar = cpu.PC  + offset; // microstate 2.
                     break;
             }
             state = FETCH_OP;
@@ -99,12 +100,12 @@ int controller(CPU_p cpu) {
                 // or get memory for load instr.
                 case OP_ADD:
                 case OP_AND:
-                    dr = cpu.ir & MASK_DR;
-                    dr = dr >> 9;
-                    sr1 = cpu.ir & MASK_SR1;
-                    sr1 = sr1 >> 6;
-                    bit5 = cpu.ir & MASK_BIT5;
-                    bit5 = bit5 >> 5;
+                    dr   = cpu.ir  & MASK_DR;
+                    dr   = dr     >> BITSHIFT_DR;
+                    sr1  = cpu.ir  & MASK_SR1;
+                    sr1  = sr1    >> BITSHIFT_SR1;
+                    bit5 = cpu.ir  & MASK_BIT5;
+                    bit5 = bit5   >> BITSHIFT_BIT5;
                     if (bit5 == 0) {
                         sr2 = cpu.ir & MASK_SR2; // no shift needed.
                     } else if (bit5 == 1) {
@@ -113,35 +114,34 @@ int controller(CPU_p cpu) {
                     // The book page 106 says current microprocessors can be done simultaneously during fetch, but this simulator is old skool.
                     break;
                 case OP_NOT:
-                    dr = cpu.ir & MASK_DR;
-                    dr = dr >> 9;
-                    sr1 = cpu.ir & MASK_SR1;
-                    sr1 = sr1 >> 6;
+                    dr  = cpu.ir  & MASK_DR;
+                    dr  = dr     >> 9;
+                    sr1 = cpu.ir  & MASK_SR1;
+                    sr1 = sr1    >> 6;
                     break;
                 case OP_TRAP:
                     vector8 = cpu.ir & MASK_TRAPVECT8; // No shift needed.
                     break;
                 case OP_LD:
-                    dr = cpu.ir & MASK_DR;
-                    dr = dr >> 9;
-                    offset = cpu.ir & MASK_PCOFFSET9;
-                    
-                    offset = SEXT(offset);
+                    dr      = cpu.ir & MASK_DR;
+                    dr      = dr    >> BITSHIFT_DR;
+                    offset  = cpu.ir & MASK_PCOFFSET9;
+                    offset  = SEXT(offset);
                     cpu.mar = cpu.PC + offset;
                     cpu.mdr = memory[cpu.mar];
                     break;
                 case OP_ST: // Same as LD.
                     // Book page 124.
-                    cpu.mdr = memory[cpu.reg[dr]];
+                    cpu.mdr = cpu.reg[dr]; //memory[cpu.reg[dr]];
                     break;
                 case OP_JMP:
                     sr1 = cpu.ir & MASK_SR1;
-                    sr1 = sr1 >> 6;
+                    sr1 = sr1   >> BITSHIFT_SR1;
                     break;
                 case OP_BR:
-                    cpu.cc = cpu.ir & MASK_NZP;
-                    cpu.cc = cpu.cc >> 9;
-                    offset = cpu.ir & MASK_PCOFFSET9;
+                    cpu.cc = cpu.ir  & MASK_NZP;
+                    cpu.cc = cpu.cc >> BITSHIFT_CC;
+                    offset = cpu.ir  & MASK_PCOFFSET9;
                     break;
                 default:
                     break;
@@ -153,33 +153,33 @@ int controller(CPU_p cpu) {
             switch (opcode) {
                 case OP_ADD:
                     if (bit5 == 0) {
-                        cpu.mdr = memory[cpu.reg[sr2]] + memory[cpu.reg[sr1]]; // Is result actually the MDR?
+                        cpu.mdr = cpu.reg[sr2] + cpu.reg[sr1]; //memory[cpu.reg[sr2]] + memory[cpu.reg[sr1]];
                     } else if (bit5 == 1) {
-                        cpu.mdr = memory[cpu.reg[sr1]] + immed;
+                        cpu.mdr = cpu.reg[sr1] + immed; // memory[cpu.reg[sr1]]
                     }
                     condition = getConditionCode(cpu.mdr);
                     break;
                 case OP_AND:
                     if (bit5 == 0) {
-                        cpu.mdr = memory[cpu.reg[sr2]] & memory[cpu.reg[sr1]];
+                        cpu.mdr = cpu.reg[sr2] & cpu.reg[sr1]; //memory[cpu.reg[sr2]] & memory[cpu.reg[sr1]];
                     } else if (bit5 == 1) {
-                        cpu.mdr = memory[cpu.reg[sr1]] & immed;
+                        cpu.mdr = cpu.reg[sr1] & immed; // memory[cpu.reg[sr1]]
                     }
                     break;
                 case OP_NOT:
-                    cpu.mdr = ~memory[cpu.reg[sr1]];
+                    cpu.mdr = ~cpu.reg[sr1]; // ~memory[cpu.reg[sr1]];
                     break;
                 case OP_TRAP:
                     // Book page 222.
-                    vector16 = ZEXT(vector8); // TODO: should we make this actually do a zero extend to 16 bits?
-                    cpu.mar = vector16;
-                    memory[cpu.reg[7]] = cpu.PC; // Store the PC in R7 before loading PC with the starting address of the service routine.
-                    cpu.mdr = memory[cpu.mar]; // read the contents of the register.
-                    cpu.PC = cpu.mdr; // The contents of the MDR are loaded into the PC.  Load the PC with the starting address of the service routine.
+                    vector16   = ZEXT(vector8); // TODO: should we make this actually do a zero extend to 16 bits?
+                    cpu.mar    = vector16;
+                    cpu.reg[7] = cpu.PC; // memory[cpu.reg[7]] // Store the PC in R7 before loading PC with the starting address of the service routine.
+                    cpu.mdr    = memory[cpu.mar]; // read the contents of the register.
+                    cpu.PC     = cpu.mdr; // The contents of the MDR are loaded into the PC.  Load the PC with the starting address of the service routine.
                     trap(vector8, cpu);
                     break;
                 case OP_JMP:
-                    cpu.PC = memory[cpu.reg[sr1]];
+                    cpu.PC = cpu.reg[sr1]; //memory[cpu.reg[sr1]];
                     break;
                 case OP_BR:
                      if ((cpu.cc == condition) || (cpu.cc == CONDITION_NZP)) {
@@ -197,10 +197,10 @@ int controller(CPU_p cpu) {
             case OP_ADD:
             case OP_AND: // Same as ADD
             case OP_NOT: // Sam as AND and AND.
-                memory[cpu.reg[dr]] = cpu.mdr;
+                cpu.reg[dr] = cpu.mdr; //memory[cpu.reg[dr]] = cpu.mdr;
                 break;
             case OP_LD:
-                memory[cpu.reg[dr]] = cpu.mdr; // Load into the register.
+                cpu.reg[dr] = cpu.mdr; //memory[cpu.reg[dr]] = cpu.mdr; // Load into the register.
                 break;
             case OP_ST:
                 memory[cpu.mar] = cpu.mdr;     // Store into memory.
@@ -284,28 +284,33 @@ unsigned short ZEXT(unsigned short value) {
  */
 void displayCPU(CPU_p cpu) {
     short menuSelection = 0;
-    char *fileName[200];
+    char *fileName[FILENAME_SIZE];
     printf("Welcome to the LC-3 Simulator Simulator\n");
     printf("Registers                     Memory\n");
 
     // First 8 lines
     int i = 0;
     for(i = 0; i < 8; i++) {
-        printf("R%u: %4X", i, memory[cpu.reg[i]]);   // Registers.
-        printf("%26X: %4X\n", i+0x3000, memory[i]); // Memory.
+        printf("R%u: %4X", i, cpu.reg[i]);   // Registers. memory[cpu.reg[i]]);
+        printf("%26X: %4X\n", i+SIMULATOR_OFFSET, memory[i]); // Memory.
     }
 
     // Next 3 lines
     int j;
     for (j = 0; j < 3; j++ & i++) {
-        printf("%34X: %4X\n", i+0x3000, memory[i]);
+        printf("%34X: %4X\n", i+SIMULATOR_OFFSET, memory[i]);
     }
 
     // Next 4 lines.
-    printf("PC:  %4X    IR: %4X         %4X: %4X\n", cpu.PC, cpu.ir, i+0x3000, memory[i]);
-    printf("A:   %4X     B: %4X         %4X: %4X\n", cpu.A, cpu.B, i+0x3000, memory[i++]);
-    printf("MAR: %4X   MDR: %4X         %4X: %4X\n", cpu.PC, cpu.ir, i+0x3000, memory[i++]);
-    printf("CC:  N:%d Z:%d P:%d              %4X: %4X\n", cpu.cc >> 2 & 7, cpu.cc >> 1 & 5, cpu.cc & 1, i+0x3000, memory[i++]);
+    printf("PC:  %4X    IR: %4X         %4X: %4X\n", cpu.PC, cpu.ir, i+SIMULATOR_OFFSET, memory[i]);
+    printf("A:   %4X     B: %4X         %4X: %4X\n", cpu.A, cpu.B, i+SIMULATOR_OFFSET, memory[i++]);
+    printf("MAR: %4X   MDR: %4X         %4X: %4X\n", cpu.PC, cpu.ir, i+SIMULATOR_OFFSET, memory[i++]);
+    printf("CC:  N:%d Z:%d P:%d              %4X: %4X\n",
+            cpu.cc >> BITSHIFT_CC_BIT3 & MASK_CC_N,
+            cpu.cc >> BITSHIFT_CC_BIT2 & MASK_CC_Z,
+            cpu.cc & MASK_CC_P,
+            i+SIMULATOR_OFFSET,
+            memory[i++]);
 
     // Last 2 lines.
     printf("%34X: %4X\n", i+0x3000, memory[i++]);
@@ -334,7 +339,7 @@ void displayCPU(CPU_p cpu) {
         exit(0);
         break;
     default:
-        printf(" Invalid selection\n.");
+        printf("---Invalid selection\n.");
         displayCPU(cpu);
         break;
     }
@@ -358,7 +363,7 @@ void zeroOut(unsigned short *array, int quantity) {
 CPU_p initialize() {
     CPU_p cpu = { 0    // PC
                 , 0    // cc
-                , { 92, 93, 94, 95, 96, 97, 98, 99 } // The last 8 elements of the memory[] array.
+                , { 0, 0, 0, 0, 0, 0, 0, 0 }
                 , 0    // ir
                 , 0    // mar
                 , 0 }; // mdr
@@ -370,11 +375,11 @@ CPU_p initialize() {
     zeroOut(memory, 100);
 
     // Intentionally hard coding these values into two memory registers.
-    memory[cpu.reg[0]] = 3;
-    memory[cpu.reg[1]] = 4;
-    //memory[cpu.reg[3]] = 0xB0B0;   // Intentional simulated data.
-    //memory[4] = 0xA0A0;            // Intentional simulated data.
-    //memory[cpu.reg[0]] = 0xD0E0;     // Intentional simulated data.
+    cpu.reg[0] = 3; //memory[cpu.reg[0]] = 3;
+    cpu.reg[1] = 4; //memory[cpu.reg[1]] = 4;
+    //cpu.reg[3] = 0xB0B0;   // Intentional simulated data.
+    //memory[4]  = 0xA0A0;   // Intentional simulated data.
+    //cpu.reg[0] = 0xD0E0;   // Intentional simulated data.
     return cpu;
 }
 
--- slc3.h
+++ slc3.h
@@ -4,12 +4,16 @@
  *  Date Due: Apr 22, 2018
  *  Authors:  Sam Brendel, Tyler Shupack
  *  Problem 3,4
- *  version: 4.11a
+ *  version: 4.16c
  */
 #include <stdio.h>
 
 #ifndef SLC3_H_
 #define SLC3_H_
+
+#define MEMORY_SIZE         100
+#define FILENAME_SIZE       200
+#define SIMULATOR_OFFSET 0x3000
 
 #define FETCH     0
 #define DECODE    1
@@ -18,32 +22,43 @@
 #define EXECUTE   4
 #define STORE     5
 
-#define OP_ADD             1 // 0001 0000 0000 0000
-#define OP_AND             5 // 0101 0000 0000 0000
-#define OP_NOT             9 // 1001 0000 0000 0000
-#define OP_TRAP           15 // 1111 0000 0000 0000
-#define OP_LD              2 // 0010 0000 0000 0000
-#define OP_ST              3 // 0011 0000 0000 0000
-#define OP_JMP            12 // 1100 0000 0000 0000
-#define OP_BR              0 // 0000 0000 0000 0000
+#define OP_ADD   1 // 0001 0000 0000 0000
+#define OP_AND   5 // 0101 0000 0000 0000
+#define OP_NOT   9 // 1001 0000 0000 0000
+#define OP_TRAP 15 // 1111 0000 0000 0000
+#define OP_LD    2 // 0010 0000 0000 0000
+#define OP_ST    3 // 0011 0000 0000 0000
+#define OP_JMP  12 // 1100 0000 0000 0000
+#define OP_BR    0 // 0000 0000 0000 0000
 
-#define MASK_OPCODE    61440 // 1111 0000 0000 0000
-#define MASK_DR         3584 // 0000 1110 0000 0000
-#define MASK_SR1         448 // 0000 0001 1100 0000
-#define MASK_SR2           7 // 0000 0000 0000 0111
-#define MASK_PCOFFSET9   511 // 0000 0001 1111 1111
-#define MASK_TRAPVECT8   255 // 0000 0000 1111 1111
-#define MASK_BIT5         32 // 0000 0000 0010 0000
-#define MASK_IMMED5       31 // 0000 0000 0001 1111
-#define MASK_NZP        3584 // 0000 1110 0000 0000
+#define MASK_OPCODE  61440 // 1111 0000 0000 0000
+#define MASK_DR       3584 // 0000 1110 0000 0000
+#define MASK_SR1       448 // 0000 0001 1100 0000
+#define MASK_SR2         7 // 0000 0000 0000 0111
+#define MASK_PCOFFSET9 511 // 0000 0001 1111 1111
+#define MASK_TRAPVECT8 255 // 0000 0000 1111 1111
+#define MASK_BIT5       32 // 0000 0000 0010 0000
+#define MASK_IMMED5     31 // 0000 0000 0001 1111
+#define MASK_NZP      3584 // 0000 1110 0000 0000
+#define MASK_CC_N        7
+#define MASK_CC_Z        5
+#define MASK_CC_P        1
 
-#define CONDITION_N        4 // 0000 1000 0000 0000
-#define CONDITION_Z        2 // 0000 0100 0000 0000
-#define CONDITION_P        1 // 0000 0010 0000 0000
-#define CONDITION_NZ       6 // 0000 1100 0000 0000
-#define CONDITION_NP       5 // 0000 1010 0000 0000
-#define CONDITION_ZP       3 // 0000 0110 0000 0000
-#define CONDITION_NZP      7 // 0000 0110 0000 0000
+#define CONDITION_N   4 // 0000 1000 0000 0000
+#define CONDITION_Z   2 // 0000 0100 0000 0000
+#define CONDITION_P   1 // 0000 0010 0000 0000
+#define CONDITION_NZ  6 // 0000 1100 0000 0000
+#define CONDITION_NP  5 // 0000 1010 0000 0000
+#define CONDITION_ZP  3 // 0000 0110 0000 0000
+#define CONDITION_NZP 7 // 0000 0110 0000 0000
+
+#define BITSHIFT_OPCODE 12
+#define BITSHIFT_DR      9
+#define BITSHIFT_CC      9
+#define BITSHIFT_SR1     6
+#define BITSHIFT_BIT5    5
+#define BITSHIFT_CC_BIT3 2
+#define BITSHIFT_CC_BIT2 1
 
 struct CPUType {
 	unsigned short int PC;     // program counter.
